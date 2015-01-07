469cc16e70998980003249bef7ac0aed871ff4fc
Nico Van Zyl <nico@wixelhq.com>
2015-01-07 13:37:05 +0200

    Cleaned up grunt stuff

--- gruntfile.js
+++ gruntfile.js
@@ -30,30 +30,8 @@ module.exports = function(grunt) {
         ]
       }
     },
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['src/**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      files: ['Gruntfile.js', 'js/*.js'],
       options: {
         // options here to override JSHint defaults
         globals: {
@@ -63,22 +41,14 @@ module.exports = function(grunt) {
           document: true
         }
       }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'qunit']
     }
   });
   
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('test', ['jshint', 'qunit']);
+  grunt.registerTask('test', ['jshint']);
 
-  grunt.registerTask('default', ['copy','jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['copy','jshint']);
 
 };
